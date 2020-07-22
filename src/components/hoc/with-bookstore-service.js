321423a95c001bf9af35f27efbf30c8fe009f1b6
Игнатьев Максим Юрьевич <ignatev.my@kry.local>
2020-07-22 10:35:21 +0700

    context-service, fetch-service

--- src/components/hoc/with-bookstore-service.js
+++ src/components/hoc/with-bookstore-service.js
@@ -2,7 +2,7 @@ import React from "react";
 import { BookstoreServiceConsumer } from "../bookstore-service-context";
 
 //В корневом index.js пробрасываем сервис через поставщик-провайдер: <BookstoreServiceProvider value={bookstoreService}>
-//Получателем-провайдером контекста отлавливаем bookstoreService в текущей функции, выполняем рендер функцию, которая на вход принимает этот
+//Получателем-провайдером контекста отлавливаем bookstoreService (сервис для запроса к серверу) в текущей функции, выполняем рендер функцию, которая на вход принимает этот
 //отлавливаемый сервис (bookstoreService) и прокидывает в оборачиваемый элемент Wrapped, например в App.
 //В app оборачиваем: export default withBookstoreService()(App)и внутри функции app на вход принимаем как раз отлавливаемый сервис (bookstoreService)
 //и там его используем
